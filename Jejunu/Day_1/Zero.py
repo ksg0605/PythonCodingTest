4d5260b36019323d503ee3bec7ed890595ab51de
sunkyu Kim <yyh2337@naver.com>
2021-06-22 16:00:18 +0900

    Zero

---
+++ Jejunu/Day_1/Zero.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+z = []
+for i in range(n):
+    num = int(input())
+    if num == 0:
+        z.pop()
+    else:
+        z.append(num)
+print(sum(z))
